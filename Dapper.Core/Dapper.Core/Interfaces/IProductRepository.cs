615dabb7d75b72a108fdcf01c6030200a7ff1bb7
yossef-ashraf <yossefff2001@gmail.com>
2024-06-26 10:51:49 +0300

    Dapper use .netCore

---
+++ Dapper.Core/Dapper.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Dapper.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.Core.Interfaces
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+    }
+}
