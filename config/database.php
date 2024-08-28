22dc441609f7979d56244541e86cfaabb07ec16f
yossef-ashraf <yossefff2001@gmail.com>
2024-08-28 18:27:21 +0300

    database configuration

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// config/database.php
+return [
+    'driver' => 'mysql', 
+    'host' => '127.0.0.1',
+    'port' => 3306, 
+    'dbname' => 'laravel',
+    'user' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4', 
+];
